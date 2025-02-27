98e7595b4b88e3a22ce170409022d5faff148f6a
23DP3DBesp <a230004db@rvt.lv>
2025-02-27 16:25:49 +0300

    Final

--- src/main.js
+++ src/main.js
@@ -1,35 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const changeCarBtn = document.getElementById("changeCar");
-    const factBtn = document.getElementById("loadFact");
-    const factText = document.getElementById("fact");
-
-    changeCarBtn?.addEventListener("click", changeBackground);
-
-    if (factBtn && factText) {
-        factBtn.addEventListener("click", fetchCatFact);
-        fetchCatFact();
-    }
-
-    function fetchCatFact() {
-        fetch("https://catfact.ninja/fact")
-            .then(response => response.json())
-            .then(data => factText.textContent = data.fact)
-    }
-
-    function changeBackground() {
-        let images = [
-            "src/image/f90.png",
-            "src/image/svj.png",
-            "src/image/rs7.png",
-            "src/image/e39.png",
-            "src/image/e38.png",
-            "src/image/e60.png",
-        ];
-
-        let randomIndex = Math.floor(Math.random() * images.length);
-        document.body.style.backgroundImage = `url('${images[randomIndex]}')`;
-        document.body.style.backgroundSize = "cover";
-        document.body.style.backgroundPosition = "center"
-        document.body.style.backgroundRepeat = "no-repeat";
-    }
-});
+function getCATFact() {
+    fetch("https://catfact.ninja/fact")
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("cat-fact").innerText = data.fact;
+        })
+        .catch(error => console.error(error));
+}
